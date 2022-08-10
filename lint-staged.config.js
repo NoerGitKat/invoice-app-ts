d14cfb98842bf4d86a9a2dee4b6b72230c8049e0
Noer Paanakker <noer@lightworksweb.nl>
2022-08-10 09:44:13 +0200

    build: base project

---
+++ lint-staged.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  // Lint then format TypeScript and JavaScript files
+  "**/*.(ts|tsx|js|jsx)": () => [
+    `npx eslint --fix .`,
+    `npx prettier --write .`,
+    `git add .`,
+  ],
+
+  // Format MarkDown and JSON
+  "**/*.(md|json)": () => `npx prettier --write .`,
+};
